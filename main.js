2c896b3333ab8c9f81a1b0484d34557f48ea5751
Pamela <pamela_bhopal@hotmail.co.uk>
2021-04-03 02:41:24 +0100

    add if and else conditions into javascript

--- main.js
+++ main.js
@@ -4,6 +4,24 @@ const renderCurrentDate = () => {
   dateTime.text(displayNow);
 };
 
+const renderCalenderEvents = () => {
+  //retrieve from local
+  const plannerEvents = localStorage.getItem("plannerEvents");
+
+  if (plannerEvents !== null) {
+    const currentHour = moment().hour();
+    const timeBlocks = $(".container .row");
+    const callback = function () {
+      const timeBlockTime = Number.parseInt($(this).data("time"), 10);
+      console.log(timeBlockTime);
+    };
+    console.log(timeblocks);
+    timeblocks.each(callback);
+  } else {
+    localStorage.setItem("plannerEvents", JSON.stringify({}));
+  }
+};
+
 const onReady = () => {
   renderCurrentDate();
 };
